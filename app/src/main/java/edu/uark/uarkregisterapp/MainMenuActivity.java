2f744e7bb07208c7ffd9cec50cf201bf02a4dbd7
rkmays <rkmays@uark.edu>
2019-02-28 13:57:52 -0600

    Added alertdialogs and screen transitions

--- app/src/main/java/edu/uark/uarkregisterapp/MainMenuActivity.java
+++ app/src/main/java/edu/uark/uarkregisterapp/MainMenuActivity.java
@@ -1,7 +1,11 @@
 package edu.uark.uarkregisterapp;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenuActivity extends AppCompatActivity {
 
@@ -9,6 +13,24 @@ public class MainMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+    }
+    public void NoFunctionalityButtonOnClick(View view) {
+        new AlertDialog.Builder(this).
+                setMessage("This functionality has not yet been implemented").
+                setNegativeButton(
+                        R.string.button_ok,
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        }
+                ).
+                create().
+                show();
+    }
+
+    public void LogOutOnClick(View view) {
+        this.startActivity(new Intent(getApplicationContext(), LoginActivity.class));
     }
 
 }
